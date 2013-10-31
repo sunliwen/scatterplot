2b997960e9e7c5fb1254f6f75bcb723b04061d67
Sun Liwen <sunliwen@gmail.com>
2013-10-31 22:57:56 +0800

    add 142 data set.

--- script.js
+++ script.js
@@ -1,110 +1,149 @@
 
-// chart margin
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    //width = 960 - margin.left - margin.right,
-    width = 1920 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function initTargetIdList() {
+  $.ajax({
+    url: "/data/target_ids.json",
+  })
+  .done(function(resp) {
+    // a init dropdown list
 
-// scale of axises
-var x = d3.scale.linear()
-    //.domain([0, 13152])  // TODO, 13152 hours 548 days 18 months
-    .range([0, width]);
+    var data = [];
 
-var y = d3.scale.linear()
-    //.domain([0, 100])  // TODO, max of count of ratings
-    .range([height, 0]);
+    function format(item) { return item.text; };
 
-// higher rating come with deeper color
-var color = d3.scale.ordinal()
-    .domain(["-1", "0", "1", "2"].reverse())
-    .range(colorbrewer.YlOrRd[4].reverse());
+    $.each(resp, function(target_id, hash){
+      data.push({id: hash, text: target_id});
+    });
 
-// styling of axises
-var xAxis = d3.svg.axis().scale(x).orient("bottom");
+    $("#target_list").select2({
+      data:{ results: data, text: 'text' }
+      ,formatSelection: format
+      ,formatResult: format
+    });
 
-var yAxis = d3.svg.axis().scale(y).orient("left");
+    $("#target_list").on("change", function(e) {
+      console.log(e.val);
 
-// init svg into the given div
-var svg = d3.select("#div1").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      drawPlot(e.val);
+    });
 
-// loading
-svg.append("text")
-    .attr("class", "loading")
-    .text("Loading ...")
-    .attr("x", function () { return width/2; })
-    .attr("y", function () { return height/2-5; });
-
-d3.tsv("loose.tsv", function(data) {
-
-  // Coerce the strings to numbers.
-  data.forEach(function(d) {
-    //console.log(d);
-    d.x = +d.date_hour;
-    d.y = +d.count;
-    //d.rating = +d.rating;
   });
 
-  // Compute the scales’ domains.
-  x.domain(d3.extent(data, function(d) { return d.x; })).nice();
-  y.domain(d3.extent(data, function(d) { return d.y; })).nice();
+}
 
-  // Add the x-axis.
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Time span (hour)");;
+function drawPlot(target_id) {
+  // chart margin
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      //width = 960 - margin.left - margin.right,
+      width = 1920 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-  // Add the y-axis.
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Rating (s)");
+  // scale of axises
+  var x = d3.scale.linear()  // TODO, 13152 hours 548 days 18 months
+      .range([0, width]);
 
-  svg.selectAll(".loading").remove();
+  var y = d3.scale.linear()
+      .range([height, 0]);
 
-  // Add the points!
-  svg.selectAll(".series")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .style("fill", function(d) { return color(d.rating); })
-      .attr("r", 2)
-      .attr("cx", function(d) { return x(d.x); })
-      .attr("cy", function(d) { return y(d.y); });
+  // higher rating come with deeper color
+  var color = d3.scale.ordinal()
+      .domain(["-1", "0", "1", "2"].reverse())
+      .range(colorbrewer.YlOrRd[4].reverse());
 
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + (+i + 1) * 20 + ")"; });
+  // styling of axises
+  var xAxis = d3.svg.axis().scale(x).orient("bottom");
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+  var yAxis = d3.svg.axis().scale(y).orient("left");
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
+  // init svg into the given div
+  if ($("#canvas").length!=0) {
+    d3.select("#canvas").remove();
+  }
+  $("#plot_container").html('<div id="canvas"></div>');
+  var svg = d3.select("#canvas")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-});
+  // loading
+  svg.append("text")
+      .attr("class", "loading")
+      .text("Loading ...")
+      .attr("x", function () { return width/2; })
+      .attr("y", function () { return height/2-5; });
+
+  d3.tsv("data/"+target_id+".tsv", function(data) {
+
+    // Coerce the strings to numbers.
+    data.forEach(function(d) {
+      //console.log(d);
+      d.x = +d.date_hour;
+      d.y = +d.count;
+      //d.rating = +d.rating;
+    });
+
+    // Compute the scales’ domains.
+    x.domain(d3.extent(data, function(d) { return d.x; })).nice();
+    y.domain(d3.extent(data, function(d) { return d.y; })).nice();
+
+    // Add the x-axis.
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Time span (hour)");;
+
+    // Add the y-axis.
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Rating (s)");
+
+    svg.selectAll(".loading").remove();
+
+    // Add the points!
+    svg.selectAll(".series")
+        .data(data)
+      .enter().append("circle")
+        .attr("class", "dot")
+        .style("fill", function(d) { return color(d.rating); })
+        .attr("r", 2)
+        .attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return y(d.y); });
+
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + (+i + 1) * 20 + ")"; });
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
+
+  });
+
+}
+
+
