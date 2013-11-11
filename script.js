819d573d4cb5ea161eae4e0e191ff0ccfc790b61
Sun Liwen <sunliwen@gmail.com>
2013-11-11 17:23:30 +0800

    draw multiple plots in one page.

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 
+var target_plots = [];
+
 function initTargetIdList() {
   $.ajax({
-    url: "data/target_ids.json",
+    url: "data/target_ids.json"
   })
   .done(function(resp) {
     // a init dropdown list
 
     var data = [];
 
-    function format(item) { return item.text; };
+    function format(item) { return item.text; }
 
     $.each(resp, function(target_id, hash){
       data.push({id: hash, text: target_id});
     });
 
     $("#target_list").select2({
-      data:{ results: data, text: 'text' }
+      data: { results: data, text: 'text' }
       ,formatSelection: format
       ,formatResult: format
     });
 
     $("#target_list").on("change", function(e) {
       console.log(e.val);
+      console.log(e.added);
 
-      drawPlot(e.val);
+      if (e.added) {
+        console.log("e.added");
+        if ( -1 === $.inArray(e.added.id, target_plots)){
+          console.log("create new plot");
+          target_plots.push(e.added.id);
+          drawPlot(e.added.id);
+        }
+      } else {
+        console.log("!e.added");
+      }
     });
-
   });
-
 }
 
 // chart margin
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     //width = 960 - margin.left - margin.right,
-    width = 1920 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 1400 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom;
 
 // scale of axises
 var x = d3.scale.linear()  // TODO, 13152 hours 548 days 18 months
@@ -53,14 +63,17 @@ var xAxis = d3.svg.axis().scale(x).orient("bottom");
 
 var yAxis = d3.svg.axis().scale(y).orient("left");
 
-function drawPlot(target_id) {
-
-  // init svg into the given div
-  if ($("#canvas").length!=0) {
-    d3.select("#canvas").remove();
+function initCanvas(){
+  if ($(".js-canvas").length!==0) {
+    d3.select(".js-canvas").remove();
   }
-  $("#plot_container").html('<div id="canvas"></div>');
-  var svg = d3.select("#canvas")
+}
+
+function drawPlot(target_id_hash) {
+  // init svg into the given div
+  var canvas_id = "canvas-" + target_id_hash;
+  $("#plot_container").append('<div id="'+canvas_id+'" class="js-canvas"></div>');
+  var svg = d3.select("#"+canvas_id)
     .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -74,7 +87,7 @@ function drawPlot(target_id) {
       .attr("x", function () { return width/2; })
       .attr("y", function () { return height/2-5; });
 
-  d3.tsv("data/"+target_id+".tsv", function(data) {
+  d3.tsv("data/"+target_id_hash+".tsv", function(data) {
 
     // Coerce the strings to numbers.
     data.forEach(function(d) {
@@ -82,7 +95,7 @@ function drawPlot(target_id) {
       d.x = +d.date_hour;
       d.y = +d.count;
       //d.rating = +d.rating;
-      console.log(d);
+      //console.log(d);
     });
 
     // Compute the scales’ domains.
@@ -99,7 +112,7 @@ function drawPlot(target_id) {
         .attr("x", width)
         .attr("y", -6)
         .style("text-anchor", "end")
-        .text("Time span (hour)");;
+        .text("Time span (hour)");
 
     // Add the y-axis.
     svg.append("g")
